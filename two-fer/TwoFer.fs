7841ae494ebc476d3d66eb0df959a8ba9d19d4d1
nikmerlock97 <nikmerlock97@gmail.com>
2020-05-23 17:31:43 -0500

    first commit

---
+++ two-fer/TwoFer.fs
@@ -0,0 +1,6 @@
+﻿module TwoFer
+
+let twoFer (input: string option) =
+    match input with
+    | Some input' -> sprintf "One for %s, one for me." input'
+    | None -> sprintf "One for you, one for me."
